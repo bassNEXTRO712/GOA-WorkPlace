01e98f3b97ddf718bc76874aa985701c5fe7e867
bassNEXTRO712 <nikaabrumia1985@gmail.com>
2025-03-12 02:26:25 -0700

    first commit

---
+++ level 92/classwork/classwork1.py
@@ -0,0 +1,11 @@
+
+def even_last(numbers):
+    result = 0;
+    for i in range(len(numbers)):
+        if i % 2 == 0:
+            result += i
+        else:
+            continue
+        
+even_last("Nikusha")
+print(sum)
