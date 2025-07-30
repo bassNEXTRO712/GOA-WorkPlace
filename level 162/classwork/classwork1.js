7ae7062bdf23d9c5b8cebe5de85a71ce53c269cb
bassNEXTRO712 <nikaabrumia1985@gmail.com>
2025-07-30 20:06:37 +0200

    'bruhh'

---
+++ level 162/classwork/classwork1.js
@@ -0,0 +1,3 @@
+//orive transportirebis fenis protokolia da orive gamoiyeneba brauwserebsa da serverebshi,aseve orive sachiroebs IP s rata imogzauron
+// sxvadasxa computerebshi, amastanave orive iyenebs 'CheckSum' s albatobas miuva tu ara momxmarebels esa tu is informacia sworad
+// aseve orive protokolia IETF-ის (Internet Engineering Task Force) -iss!!
